fa8e8687eb9b1fa5005ec849282fec26177df32c
Grisha <sibrim1212@gmail.com>
2024-03-05 19:31:05 +0400

    fix

--- internal/app/controller/http/user.go
+++ internal/app/controller/http/user.go
@@ -52,6 +52,7 @@ func (userHandler *UserHandler) RegisterHandler(w http.ResponseWriter, r *http.R
 		Name:  string("USER_ID"),
 		Value: token,
 	}
+	w.Header().Add("Content-Type", "application/json")
 	http.SetCookie(w, cookie)
 	w.WriteHeader(http.StatusOK)
 }
